5eb4c39bb2629fcaaa2a27eaf946825371dedb03
boyercb <cboyer@poverty-action.org>
2016-03-28 15:16:13 -0400

    Add program shell.

---
+++ python/infographics.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas
+
+# read data
+data = pandas.read_csv('indicators.csv')
+
+# clean and organize indicators
+
+# read and trim image files
+
+# scorecard logic
+for
+
+# export
